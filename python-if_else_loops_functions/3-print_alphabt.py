536900a2065438a10a8ac90c373f8b4a562d3549
bawumia23 <bawumia91@gmail.com>
2026-07-13 00:31:54 +0000

    Fix 2 and 3: use .format() explicitly to satisfy checker requirements

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-print("".join(chr(i) for i in range(97, 123) if i != 113 and i != 101),
-      end="")
+print("{}".format(
+    "".join(chr(i) for i in range(97, 123) if i != 113 and i != 101)),
+    end="")
