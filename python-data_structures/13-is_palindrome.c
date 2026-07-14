afa39515c23af7b35b6e452ba098b7d3d6e9c466
bawumia23 <bawumia91@gmail.com>
2026-07-14 22:13:01 +0000

    0x03: fix is_palindrome to avoid extra malloc

--- python-data_structures/13-is_palindrome.c
+++ python-data_structures/13-is_palindrome.c
@@ -1,5 +1,24 @@
-#include <stdlib.h>
 #include "lists.h"
+
+/**
+ * reverse_sub_list - reverses a singly linked sub-list in place
+ * @node: pointer to the first node of the sub-list to reverse
+ * Return: pointer to the new head of the reversed sub-list
+ */
+static listint_t *reverse_sub_list(listint_t *node)
+{
+	listint_t *prev, *next;
+
+	prev = NULL;
+	while (node != NULL)
+	{
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
+	}
+	return (prev);
+}
 
 /**
  * is_palindrome - checks if a singly linked list is a palindrome
@@ -8,34 +27,44 @@
  */
 int is_palindrome(listint_t **head)
 {
-	const listint_t *node;
-	int *values;
-	size_t size, i;
+	listint_t *slow, *fast, *prev_slow, *second_half, *p1, *p2;
+	int result;
 
 	if (head == NULL || *head == NULL)
 		return (1);
 
-	size = 0;
-	for (node = *head; node != NULL; node = node->next)
-		size++;
+	slow = *head;
+	fast = *head;
+	prev_slow = NULL;
 
-	values = malloc(sizeof(int) * size);
-	if (values == NULL)
-		return (0);
-
-	i = 0;
-	for (node = *head; node != NULL; node = node->next)
-		values[i++] = node->n;
-
-	for (i = 0; i < size / 2; i++)
+	while (fast != NULL && fast->next != NULL)
 	{
-		if (values[i] != values[size - 1 - i])
-		{
-			free(values);
-			return (0);
-		}
+		prev_slow = slow;
+		slow = slow->next;
+		fast = fast->next->next;
 	}
 
-	free(values);
-	return (1);
+	second_half = reverse_sub_list(slow);
+
+	result = 1;
+	p1 = *head;
+	p2 = second_half;
+	while (p2 != NULL)
+	{
+		if (p1->n != p2->n)
+		{
+			result = 0;
+			break;
+		}
+		p1 = p1->next;
+		p2 = p2->next;
+	}
+
+	second_half = reverse_sub_list(second_half);
+	if (prev_slow == NULL)
+		*head = second_half;
+	else
+		prev_slow->next = second_half;
+
+	return (result);
 }
