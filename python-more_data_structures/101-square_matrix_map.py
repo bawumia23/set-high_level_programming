2f9d5563d9731436af1f2b176f3047ac05643058
bawumia23 <bawumia91@gmail.com>
2026-07-15 00:14:33 +0000

    Fix pycodestyle issues

--- python-more_data_structures/101-square_matrix_map.py
+++ python-more_data_structures/101-square_matrix_map.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Module that squares all integers of a matrix, using map."""
+
+
 def square_matrix_map(matrix=[]):
     """Return a new matrix with each value squared, using map."""
     return list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
