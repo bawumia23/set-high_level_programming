1ec0e6321101cf22a3e147faf87c07368c61cb55
bawumia23 <bawumia91@gmail.com>
2026-07-26 13:12:45 +0000

    Fix 101-locked_class.py: use __slots__ instead of __setattr__ override

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -5,14 +5,7 @@
 class LockedClass:
     """Prevent dynamic creation of new instance attributes.
 
-    The only new instance attribute allowed to be created is
-    ``first_name``. Any other attribute must already exist.
+    The only instance attribute allowed to be set is ``first_name``.
     """
 
-    def __setattr__(self, name, value):
-        """Restrict attribute creation to 'first_name' only."""
-        if name != "first_name" and not hasattr(self, name):
-            raise AttributeError(
-                "'{}' object has no attribute '{}'".format(
-                    type(self).__name__, name))
-        super().__setattr__(name, value)
+    __slots__ = ('first_name',)
