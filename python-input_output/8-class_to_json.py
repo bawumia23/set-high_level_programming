e2e6167e457d408d2fd69dec15a4c773eeb6efa5
bawumia23 <bawumia91@gmail.com>
2026-08-05 14:04:13 +0000

    Update 8-class_to_json.py

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-"""Module to convert class instance to JSON serializable dict."""
+"""Module for class_to_json function."""
 
 
 def class_to_json(obj):
     """Return the dictionary description with simple data structure.
 
     The dictionary is for JSON serialization of an object.
-    """
-    Args:
-        obj: An instance of a Class.
-
-    Returns:
-        A dictionary representation of the object.
     """
     return obj.__dict__
