c9e6b376f24122ef40b5e5bf8143d43ef8a4061c
bawumia23 <bawumia91@gmail.com>
2026-08-05 13:50:37 +0000

    Update 1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,7 +3,7 @@
 
 
 def write_file(filename="", text=""):
-    """Writes a string to a text file (UTF8) and returns the number of characters written.
+    """Write a string to a text file (UTF8).
 
     Args:
         filename: The name of the file to write to.
