bacf337f717bbdf6519bfd5ee018c51684ad1670
bawumia23 <bawumia91@gmail.com>
2026-08-05 14:28:04 +0000

    Refactor docstrings in Student class

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,30 +1,20 @@
 #!/usr/bin/python3
-"""Module defining the Student class with filter."""
+"""Module for Student class."""
 
 
 class Student:
-    """Defines a student."""
+    """Student class."""
 
     def __init__(self, first_name, last_name, age):
-        """Initializes a Student instance.
-
-        Args:
-            first_name: The student's first name.
-            last_name: The student's last name.
-            age: The student's age.
-        """
+        """Initialize a Student."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieves a dictionary representation of a Student instance.
+        """Return a dictionary representation of a Student.
 
-        Args:
-            attrs: A list of attribute names to retrieve. If None, all attributes are retrieved.
-
-        Returns:
-            A dictionary representation of the Student.
+        If attrs is provided, only include those attributes.
         """
         if attrs is None:
             return self.__dict__
