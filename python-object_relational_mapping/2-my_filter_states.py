c417a758d34b8397433a3cd8a3cfcf6d6bfb0417
bawumia23 <bawumia91@gmail.com>
2026-08-16 01:13:48 +0000

    Fix case-sensitive name matching in tasks 1, 2, 3, 5

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -18,8 +18,9 @@ if __name__ == "__main__":
         charset="utf8"
     )
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
-        sys.argv[4]
+    query = (
+        "SELECT * FROM states WHERE BINARY name = '{}' "
+        "ORDER BY id ASC".format(sys.argv[4])
     )
     cur.execute(query)
     rows = cur.fetchall()
--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     cur.execute(
         "SELECT cities.name FROM cities "
         "JOIN states ON cities.state_id = states.id "
-        "WHERE states.name = %s "
+        "WHERE BINARY states.name = %s "
         "ORDER BY cities.id ASC",
         (sys.argv[4],)
     )
