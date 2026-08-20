922673de09b14e9ae0d48f8708120e7e767fe279
bawumia23 <bawumia91@gmail.com>
2026-08-20 07:19:15 +0000

    1-body.sh: follow redirects with -L before checking status

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a GET request and displays the body only if the status is 200
-[ "$(curl -s -o /dev/null -w '%{http_code}' "$1")" = "200" ] && curl -s "$1"
+# sends a GET request, follows redirects, and displays the body if status is 200
+[ "$(curl -s -L -o /dev/null -w '%{http_code}' "$1")" = "200" ] && curl -s -L "$1"
