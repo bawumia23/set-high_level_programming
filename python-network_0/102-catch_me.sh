0609801d2a6b7caf6830e214e9f2c1eed0f599ae
bawumia23 <bawumia91@gmail.com>
2026-08-20 07:31:28 +0000

    102-catch_me.sh: use POST instead of GET

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a request that makes the server respond with You got me!
-curl -s -A "curl" 0.0.0.0:5000/catch_me
+# sends a POST request that makes the server respond with You got me!
+curl -s -X POST 0.0.0.0:5000/catch_me
