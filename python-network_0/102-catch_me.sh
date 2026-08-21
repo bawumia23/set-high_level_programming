77155053d664b3419f91cdb28253c80a6d53aac1
bawumia23 <bawumia91@gmail.com>
2026-08-21 05:19:27 +0000

    102-catch_me.sh: PUT with user_id param and Origin header

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a POST request that makes the server respond with You got me!
-curl -s -X POST 0.0.0.0:5000/catch_me
+# sends a PUT request with a user_id param and Origin header to catch_me
+curl -s -L -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
